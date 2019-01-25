adeb455fb9dac1380eb922782e5d5634f2ee9928
Veriny <20imj68@myahs.org>
2019-01-24 18:02:30 -0800

    Updated Dependancies For Real

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionOPModes/VisionOP.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.VisionOPModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class VisionOP extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+    }
+}
