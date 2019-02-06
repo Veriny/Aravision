0c91aeaa104393154a9a012234875ff85f536511
Justin Im <20imj68@myahs.org>
2019-02-06 15:33:10 -0800

    Yeet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision.java
@@ -1,4 +1,12 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-public class Vision {
+import org.corningrobotics.enderbots.endercv.OpenCVPipeline;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class Vision extends OpenCVPipeline{
+    public Mat processFrame(Mat rgba, Mat gray) {
+        Mat yuv = new Mat();
+        Imgproc.cvtcolor(rgba, yuv, Imgproc.RGB);
+    }
 }
